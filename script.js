e1b0cec8e2297256fe1be475667a3464a0210a83
dhanraj-saur <121377364+dhanraj-saur@users.noreply.github.com>
2023-04-15 00:34:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 //your code here
-function countLetter(){
-	var text = document.getElementById("evaluatedText").value
-	var numWords = 0;
-	for (var i = 0; i < text.length; i++) {
-          var currentCharacter = text[i];
-                        
-         if (currentCharacter == " ") {
-                    numWords += 1;
-                }
-            }
-	return numWords;
+document.getElementById("letterCount").innerHTML = 0;
+
+document.getElementById('evaluatedText').onkeyup = function(){
+   
+	document.getElementById("letterCount").innerHTML = this.value.length;
 }
-document.getElementById("letterCount").innerHTML = numWords ;
+
+
+  
